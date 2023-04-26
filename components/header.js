a6387365a1444e7298bd45ddd2c08c713d164d5a
Wen Shan Jiang <wenshanj@andrew.cmu.edu>
2023-04-25 20:43:13 -0400

    style footer and header

--- components/header.js
+++ components/header.js
@@ -5,10 +5,8 @@ class Header extends HTMLElement {
 
     connectedCallback() {
       this.innerHTML = `
-        <style>
-        </style>
         <header>
-          <nav class="navbar navbar-light bg-light pl-5 pr-5">
+          <nav class="navbar bg-light pl-5 pr-5" style="background-color: #D9D9D9 !important;">
             <a class="navbar-brand" href="https://www.cs.cmu.edu/">
               <img src="assets/scs-unitmark.png" width="200" height="37" class="d-inline-block align-top" alt="">
             </a>
